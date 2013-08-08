99c3cfd15c2aa477060351bcf260d059bfe0014f
João Felipe Santos <joao.eel@gmail.com>
2013-08-08 09:53:40 -0400

    Added license and a function for computing the Hilbert transform.

---
+++ src/auditory.jl
@@ -0,0 +1,19 @@
+module auditory
+
+export hilbert
+
+function hilbert(x)
+# Return the Hilbert transform of x.
+# Code inspired by Scipy's implementation, which is under BSD license.
+    X = fft(x)
+    N = length(X)
+    h = zeros(N)
+    if N % 2 == 0
+        h[1] = h[N/2+1] = 1
+        h[2:N/2] = 2
+    else
+        h[1] = 1
+        h[2:(N+1)/2+1] = 2
+    end
+    return ifft(X.*h)
+end
