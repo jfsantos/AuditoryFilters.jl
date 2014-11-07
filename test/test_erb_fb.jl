514a7b733a985d7fd8d9db6833e55839ef2bd2b9
Joao Felipe Santos <joao.eel@gmail.com>
2014-11-07 11:24:44 -0500

    Removed MATLAB dependency for ERB filterbank tests, updated REQUIRE

--- test/test_erb_fb.jl
+++ test/test_erb_fb.jl
@@ -1,26 +1,29 @@
-using Auditory, MATLAB, Base.Test
+using Auditory, DSP, Base.Test
 
-# testing auditory filterbank design
-
-restart_default_msession()
-
+# testing ERB filterbank design
 fb = make_erb_filterbank(16000, 23, 150)
-
-@matlab begin
-    addpath("/Users/jfsantos/Projects/SRMR_toolbox/auditory");
-    fb_matlab = MakeERBFilters(16000, 23, 150);
+fb_matlab = readcsv(open(joinpath(dirname(@__FILE__), "data", "ERB_filter_coeffs.csv")))
+@test length(fb.filters) == size(fb_matlab, 1)
+for k=1:length(fb.filters)
+	sos = fb.filters[k]
+	biquads = sos.biquads
+	g = sos.g
+	for b=1:4
+		@test_approx_eq biquads[b].b0 fb_matlab[k,1]
+		@test_approx_eq biquads[b].b2 fb_matlab[k,6]
+		@test_approx_eq biquads[b].a1 fb_matlab[k,8]
+		@test_approx_eq biquads[b].a2 fb_matlab[k,9]
+	end	 
+	@test_approx_eq biquads[1].b1 fb_matlab[k,2]
+	@test_approx_eq biquads[2].b1 fb_matlab[k,3]
+	@test_approx_eq biquads[3].b1 fb_matlab[k,4]
+	@test_approx_eq biquads[4].b1 fb_matlab[k,5]
+	@test_approx_eq 1/g fb_matlab[k,10]
 end
 
-@mget fb_matlab
-
-x = zeros(1000)
+# testing ERB filterbank response
+x = zeros(10000)
 x[1] = 1.0
 y = filt(fb, x)
-
-@mput x
-@matlab y_matlab = ERBFilterBank(x, fb_matlab)
-@mget y_matlab
-
-close_default_msession()
-
+y_matlab = readcsv(open(joinpath(dirname(@__FILE__), "data", "ERB_filter_response.csv")))
 @test_approx_eq y y_matlab'
