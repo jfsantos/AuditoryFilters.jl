d0a1fac962faf3ab35f6c4ef05c45a6b888b8827
Joao Felipe Santos <joao.eel@gmail.com>
2014-11-07 18:18:07 -0500

    Importing Base.filt and adding runtests.jl

--- src/Auditory.jl
+++ src/Auditory.jl
@@ -1,7 +1,7 @@
 module Auditory
 
 using DSP
-import Base: convert
+import Base: convert, filt
 export erb_space, make_erb_filterbank, erb_filterbank, compute_modulation_cfs, make_modulation_filter, modulation_filterbank, gammatonegram
 
 include("ERBFilterbank.jl")
---
+++ test/runtests.jl
@@ -0,0 +1,3 @@
+include("test_erb_fb.jl")
+include("test_modulation_fb.jl")
+include("test_gammatonegram.jl")
