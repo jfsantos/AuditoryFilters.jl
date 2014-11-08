34fd9c6310002fc707621a702ea53a90eff3bcfd
Joao Felipe Santos <joao.eel@gmail.com>
2014-11-08 17:00:24 -0500

    Exporting immutable types ERBFilterbank and ModulationFilterbank

--- src/Auditory.jl
+++ src/Auditory.jl
@@ -2,7 +2,8 @@ module Auditory
 
 using DSP
 import Base: convert, filt
-export erb_space, make_erb_filterbank, erb_filterbank, compute_modulation_cfs, make_modulation_filter, modulation_filterbank, gammatonegram
+export erb_space, make_erb_filterbank, erb_filterbank, compute_modulation_cfs, make_modulation_filter, modulation_filterbank, gammatonegram, 
+	   ERBFilterbank, ModulationFilterbank
 
 include("ERBFilterbank.jl")
 include("ModulationFilterbank.jl")
