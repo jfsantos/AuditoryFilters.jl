074e92967d1743b20dd2cfaebcc203fca992a70e
Elliot Saba <staticfloat@gmail.com>
2017-10-03 12:37:13 -0700

    Cleanup exports

--- src/AuditoryFilters.jl
+++ src/AuditoryFilters.jl
@@ -3,8 +3,9 @@ module AuditoryFilters
 
 using DSP
 import Base: convert, filt
-export erb_space, make_erb_filterbank, erb_filterbank, compute_modulation_cfs, make_modulation_filter, modulation_filterbank, gammatonegram,
-       ERBFilterbank, ModulationFilterbank
+export erb_space, make_erb_filterbank, compute_modulation_cfs, make_modulation_filter,
+       modulation_filterbank, gammatonegram, fft2gammatonemx,
+       ERBFilterbank, ModulationFilterbank, 
 
 abstract type Filterbank end
 
